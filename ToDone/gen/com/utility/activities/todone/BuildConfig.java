7117a9804edea1038aa728d023f33f04e6947367
chrslgn <chrs.lgn@gmail.com>
2014-04-09 18:33:46 -0400

    Duplicated expandablelist tutorial for reference
    Duplicated
    http://www.androidhive.info/2013/07/android-expandable-list-view-tutorial/
    as a scaffold for an expandable list

--- ToDone/gen/com/utility/activities/todone/BuildConfig.java
+++ ToDone/gen/com/utility/activities/todone/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package com.utility.todone;
+package com.utility.activities.todone;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
--- ToDone/gen/com/utility/activities/todone/R.java
+++ ToDone/gen/com/utility/activities/todone/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package com.utility.todone;
+package com.utility.activities.todone;
 
 public final class R {
     public static final class attr {
@@ -14,10 +14,16 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int LinearLayout1=0x7f070000;
+        public static final int expandable_list=0x7f070001;
+        public static final int lblListHeader=0x7f070002;
+        public static final int lblListItem=0x7f070003;
+        public static final int menu_settings=0x7f070004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int list_group=0x7f030001;
+        public static final int list_item=0x7f030002;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
